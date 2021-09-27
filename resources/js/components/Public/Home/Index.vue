0fe62b1f42185ff96d6615d7a72a9a2336d0c708
svardys <75584296+svardys@users.noreply.github.com>
2021-09-27 20:10:03 +0200

    homepage

--- resources/js/components/Public/Home/Index.vue
+++ resources/js/components/Public/Home/Index.vue
@@ -1,5 +1,108 @@
 <template>
-    <div>
-        Hello world
+  <div class="relative bg-white pt-10 pb-32 overflow-hidden">
+    <div class="relative">
+      <div class="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:grid-flow-col-dense lg:gap-24">
+        <div class="px-4 max-w-xl mx-auto sm:px-6 lg:py-16 lg:max-w-none lg:mx-0 lg:px-0">
+          <div>
+            <div>
+              <span class="h-12 w-12 rounded-md flex items-center justify-center bg-indigo-600">
+                <InboxIcon class="h-6 w-6 text-white" aria-hidden="true" />
+              </span>
+            </div>
+            <div class="mt-6">
+              <h2 class="text-3xl font-extrabold tracking-tight text-gray-900">
+                Kuchyně na míru
+              </h2>
+              <p class="mt-4 text-lg text-gray-500">
+                Semper curabitur ullamcorper posuere nunc sed. Ornare iaculis bibendum malesuada faucibus lacinia porttitor. Pulvinar laoreet sagittis viverra duis. In venenatis sem arcu pretium pharetra at. Lectus viverra dui tellus ornare pharetra.
+              </p>
+              <div class="mt-6">
+                <a href="#" class="inline-flex px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700">
+                  Pokračovat na galerii
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="mt-12 sm:mt-16 lg:mt-0">
+          <div class="pl-4 -mr-48 sm:pl-6 md:-mr-16 lg:px-0 lg:m-0 lg:relative lg:h-full">
+            <img class="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:left-0 lg:h-full lg:w-auto lg:max-w-none" src="https://cdn.pixabay.com/photo/2016/04/18/08/50/kitchen-1336160_960_720.jpg" alt="Inbox user interface" />
+          </div>
+        </div>
+      </div>
     </div>
+    <div class="mt-24">
+      <div class="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:grid-flow-col-dense lg:gap-24">
+        <div class="px-4 max-w-xl mx-auto sm:px-6 lg:py-32 lg:max-w-none lg:mx-0 lg:px-0 lg:col-start-2">
+          <div>
+            <div>
+              <span class="h-12 w-12 rounded-md flex items-center justify-center bg-indigo-600">
+                <SparklesIcon class="h-6 w-6 text-white" aria-hidden="true" />
+              </span>
+            </div>
+            <div class="mt-6">
+              <h2 class="text-3xl font-extrabold tracking-tight text-gray-900">
+                Vestavěnné skříně
+              </h2>
+              <p class="mt-4 text-lg text-gray-500">
+                Semper curabitur ullamcorper posuere nunc sed. Ornare iaculis bibendum malesuada faucibus lacinia porttitor. Pulvinar laoreet sagittis viverra duis. In venenatis sem arcu pretium pharetra at. Lectus viverra dui tellus ornare pharetra.
+              </p>
+              <div class="mt-6">
+                <a href="#" class="inline-flex px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700">
+                  Pokračovat na galerii
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="mt-12 sm:mt-16 lg:mt-0 lg:col-start-1">
+          <div class="pr-4 -ml-48 sm:pr-6 md:-ml-16 lg:px-0 lg:m-0 lg:relative lg:h-full">
+            <img class="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:right-0 lg:h-full lg:w-auto lg:max-w-none" src="https://cdn.pixabay.com/photo/2017/07/09/03/19/home-2486092_960_720.jpg" alt="Customer profile user interface" />
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="relative mt-24">
+      <div class="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:grid-flow-col-dense lg:gap-24">
+        <div class="px-4 max-w-xl mx-auto sm:px-6 lg:py-16 lg:max-w-none lg:mx-0 lg:px-0">
+          <div>
+            <div>
+              <span class="h-12 w-12 rounded-md flex items-center justify-center bg-indigo-600">
+                <InboxIcon class="h-6 w-6 text-white" aria-hidden="true" />
+              </span>
+            </div>
+            <div class="mt-6">
+              <h2 class="text-3xl font-extrabold tracking-tight text-gray-900">
+                Komeční prostory
+              </h2>
+              <p class="mt-4 text-lg text-gray-500">
+                Semper curabitur ullamcorper posuere nunc sed. Ornare iaculis bibendum malesuada faucibus lacinia porttitor. Pulvinar laoreet sagittis viverra duis. In venenatis sem arcu pretium pharetra at. Lectus viverra dui tellus ornare pharetra.
+              </p>
+              <div class="mt-6">
+                <a href="#" class="inline-flex px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700">
+                  Pokračovat na galerii
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="mt-12 sm:mt-16 lg:mt-0">
+          <div class="pl-4 -mr-48 sm:pl-6 md:-mr-16 lg:px-0 lg:m-0 lg:relative lg:h-full">
+            <img class="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:left-0 lg:h-full lg:w-auto lg:max-w-none" src="https://cdn.pixabay.com/photo/2017/07/09/03/19/home-2486092_960_720.jpg" alt="Inbox user interface" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+import { InboxIcon, SparklesIcon } from '@heroicons/vue/outline'
+
+export default {
+  components: {
+    InboxIcon,
+    SparklesIcon,
+  },
+}
+</script>
