06fa1a68cb9f882a1caf47269af2466891ef58d2
svardys <75584296+svardys@users.noreply.github.com>
2021-10-20 21:02:44 +0200

    footer

--- resources/js/components/Auth/Logout.vue
+++ resources/js/components/Auth/Logout.vue
@@ -1,5 +1,7 @@
 <template>
-    <div @click="logout">Odhlášení</div>
+    <button @click="logout" type="button" class="mt-2 ml-2 inline-flex items-center px-3.5 py-2 border border-transparent text-sm leading-4 font-medium rounded-full shadow-sm text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+        Odhlásit se
+    </button>
 </template>
 
 <script>
