b927377fb6c5505c64a996d941f41f467f7c09b6
svardys <75584296+svardys@users.noreply.github.com>
2021-10-20 19:15:32 +0200

    Admin items

--- resources/js/components/Admin/Gallery/Index.vue
+++ resources/js/components/Admin/Gallery/Index.vue
@@ -11,7 +11,7 @@
             </h3>
             <div class="mt-4 sm:mt-0 sm:ml-10">
                 <nav class="-mb-px flex space-x-8">
-                <a v-for="tab in tabs" :key="tab.name" @click="tab.current=!tab.current" :href="tab.href" :class="[tab.current ? 'border-indigo-500 text-indigo-600' : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300', 'whitespace-nowrap pb-4 px-1 border-b-2 font-medium text-sm']" :aria-current="tab.current ? 'page' : undefined">
+                <a v-for="(tab, i) in tabs" :key="i" @click="selectItem(i)" :href="tab.href" :class="[i === activeItem ? 'border-indigo-500 text-indigo-600' : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300', 'whitespace-nowrap pb-4 px-1 border-b-2 font-medium text-sm']" :aria-current="tab.current ? 'page' : undefined">
                     {{ tab.name }}
                 </a>
                 </nav>
@@ -203,11 +203,18 @@ export default {
     },
     setup() {
         const open = ref(false);
+        const activeItem = 0;
         return{
             galeries,
             open,
             tabs,
+            activeItem,
         }
+    },
+    methods: {
+        selectItem(i) {
+            this.activeItem = i;
+        },
     }
 }
 </script>
