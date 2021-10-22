383d9c2c881b6d2b33d9aa4154bab7ba5b5fbe93
svardys <75584296+svardys@users.noreply.github.com>
2021-10-22 14:51:55 +0200

    Final front create

--- resources/js/components/Admin/Gallery/Create.vue
+++ resources/js/components/Admin/Gallery/Create.vue
@@ -50,10 +50,13 @@
                                        class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">Úvodní
                                     obrázek</label>
                                 <div class="mt-1 sm:mt-0 sm:col-span-2">
-                                    <div
-                                        class="max-w-lg flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
+                                    <div id="thumbn" class="max-w-lg flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md" @dragover="dragover" @dragleave="dragleave" @drop="drop">
                                         <div class="space-y-1 text-center">
-                                            <svg class="mx-auto h-12 w-12 text-gray-400" stroke="currentColor"
+                                            <div v-if="thumbnailPreview" @click="remove('thumb')" class="relative group block w-full aspect-w-10 aspect-h-7 rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-offset-gray-100 focus-within:ring-indigo-500 overflow-hidden">
+                                                <img :src="thumbnailPreview" alt="" class="object-cover pointer-events-none group-hover:opacity-75" />
+                                                <XIcon class="absolute inset-0 visible w-full z-10 cursor-pointer text-gray-900 opacity-0 group-hover:opacity-100"></XIcon>
+                                            </div>
+                                            <svg v-else class="mx-auto h-12 w-12 text-gray-400" stroke="currentColor"
                                                  fill="none" viewBox="0 0 48 48" aria-hidden="true">
                                                 <path
                                                     d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
@@ -72,9 +75,7 @@
                                         </div>
                                     </div>
                                     <div class="mt-1 text-sm text-red-600" v-if="form.errors.has('thumbnail')"
-                                         v-text="form.errors.get('thumbnail')"></div>
-                                    <div v-if="thumbnailPreview">
-                                        <img :src="thumbnailPreview" class="preview" style="width:600px;" alt=""/>
+                                         v-text="form.errors.get('thumbnail')">
                                     </div>
                                 </div>
                             </div>
@@ -84,8 +85,7 @@
                                        class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">Další
                                     obrázky</label>
                                 <div class="mt-1 sm:mt-0 sm:col-span-2">
-                                    <div
-                                        class="max-w-lg flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md" @dragover="dragover" @dragleave="dragleave" @drop="drop">
+                                    <div id="image" class="max-w-lg flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md" @dragover="dragover" @dragleave="dragleave" @drop="drop">
                                         <div class="space-y-1 text-center">
                                             <svg class="mx-auto h-12 w-12 text-gray-400" stroke="currentColor"
                                                  fill="none" viewBox="0 0 48 48" aria-hidden="true">
@@ -95,7 +95,7 @@
                                             </svg>
                                             <div class="flex text-sm text-gray-600">
                                                 <label for="images"
-                                                       class="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
+                                                       class="relative cursor-pointer rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
                                                     <span>Nahrát soubor</span>
                                                     <input @change="showImages" id="images" name="images" type="file"
                                                            multiple accept="image/png, image/jpeg" class="sr-only"/>
@@ -166,13 +166,27 @@ export default {
         }))
 
         function showThumbnail(e) {
-            let selectedFile = e.target.files[0]
+            let selectedFile = '';
+            if (e.target.files === undefined ){
+                selectedFile = e.dataTransfer.files[0];
+            }
+            else
+            {
+                selectedFile = e.target.files[0];
+            }
             thumbnailPreview.value = URL.createObjectURL(selectedFile)
             form.thumbnail = selectedFile
         }
 
         function showImages(e) {
-            let selectedFiles = e.target.files || e.dataTransfer.files;
+            let selectedFiles = '';
+            if (e.target.files === undefined ){
+                selectedFiles = e.dataTransfer.files;
+            }
+            else
+            {
+                selectedFiles = e.target.files;
+            }
             for (let i = 0; i < selectedFiles.length; i++) {
                 let img = {
                     src: URL.createObjectURL(selectedFiles[i]),
@@ -204,14 +218,20 @@ export default {
 
         //Drag AND Drop
         function remove(i) {
-            form.images.splice(i, 1);
-            imagesPreview.value.splice(i, 1)
+            if (i === 'thumb'){
+                form.thumbnail = "";
+                thumbnailPreview.value = "";
+            }
+            else
+            {
+                form.images.splice(i, 1);
+                imagesPreview.value.splice(i, 1)
+            }
         }
 
 
         function dragover(event) {
             event.preventDefault();
-            // Když na to najede
             if (!event.currentTarget.classList.contains('bg-green-300')) {
                 event.currentTarget.classList.remove('bg-white');
                 event.currentTarget.classList.add('bg-green-300');
@@ -219,14 +239,19 @@ export default {
         }
 
         function dragleave(event) {
-            // Když z toho vyjede
             event.currentTarget.classList.add('bg-white');
             event.currentTarget.classList.remove('bg-green-300');
         }
 
         function drop(event) {
             event.preventDefault();
-            showImages(event);
+            if (event.target.id === 'image'){
+                showImages(event);
+            }
+            else
+            {
+                showThumbnail(event);
+            }
             event.currentTarget.classList.add('bg-white');
             event.currentTarget.classList.remove('bg-green-300');
         }
