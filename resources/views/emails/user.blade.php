6f996f30d9077bc9287c86d8004d5aa95da10937
dfd <mcvespr@gmail.com>
2021-10-28 15:52:04 +0200

    Blade

--- resources/views/emails/user.blade.php
+++ resources/views/emails/user.blade.php
@@ -1,20 +1,18 @@
 <html>
-    <head>
-        <title>Zpráva uživateli</title>
-    </head>
-    <body>
-        <div>
-            <p>Dobrý den,</p> 
-            <p>Děkujeme že jste nás kontaktovali. Budeme se Vám snažit odpovědět co nejdříve.</p>
-            <p>S přáním pěkného dne TRUWORK S.R.O</p>
-            <div>
-                <p>
-                    <p>Adresa  : Jeseník 790 00, Astur 185</p>
-                    <p>Telefon : 607 238 514</p>
-                    <p>Email   : truwork@seznam.cz</p>
-                </p>
-            </div>
-            <img src="" . {{ $mail_data['logo'] }} . "" alt="" width="100px" height="50px" />
-        </div>
-    </body>
+<head>
+    <title>Zpráva uživateli</title>
+</head>
+<body>
+<div>
+    <p>Dobrý den,</p>
+    <p>Děkujeme že jste nás kontaktovali. Budeme se Vám snažit odpovědět co nejdříve.</p>
+    <p>S přáním pěkného dne TRUWORK S.R.O</p>
+    <div>
+        <p>Adresa : Jeseník 790 00, Astur 185</p>
+        <p>Telefon : 607 238 514</p>
+        <p>Email : truwork@seznam.cz</p>
+    </div>
+    <img src="{{ $mail_data['logo'] }}" alt="" width="100px" height="50px"/>
+</div>
+</body>
 </html>
