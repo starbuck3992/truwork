56be870364f5f0cfa4f34bc2e73d89dc73e0cf36
svardys <75584296+svardys@users.noreply.github.com>
2021-12-03 19:31:27 +0100

    FRONT - SHIT

--- resources/js/components/Public/Gallery/Index.vue
+++ resources/js/components/Public/Gallery/Index.vue
@@ -1,6 +1,6 @@
 <template>
-    <HeroScene :title="category"></HeroScene>
-    <h1 class="mt-10 text-4xl font-extrabold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl text-center"> Ukázka naší práce</h1>
+    <!-- <HeroScene :title="category"></HeroScene>
+    <h1 class="text-4xl font-extrabold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl text-center"> Ukázka naší práce</h1>
     <div class="relative bg-mycolor max-w-7xl mx-auto py-8 px-4 sm:px-6 lg:px-8 mt-5">
         <ul role="list" class="grid grid-cols-2 gap-x-3 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-3 xl:gap-x-8">
             <li v-for="(gallery, i) in galleries" :key="i" class="relative">
@@ -17,7 +17,120 @@
                 </router-link>
             </li>
         </ul>
-    </div>
+    </div> -->
+    <main>
+      <!-- Hero section -->
+      <div class="relative py-4 my-4">
+        <!-- Background image and overlap -->
+        <div aria-hidden="true" class="hidden absolute inset-0 sm:flex sm:flex-col">
+          <div class="flex-1 relative w-full bg-gray-800">
+            <div class="absolute inset-0 overflow-hidden">
+              <img src="https://tailwindui.com/img/ecommerce-images/home-page-04-hero-full-width.jpg" alt="" class="w-full h-full object-center object-cover">
+            </div>
+            <div class="absolute inset-0 bg-gray-900 opacity-50"></div>
+          </div>
+          <div class="w-full bg-white h-32 md:h-40 lg:h-48"></div>
+        </div>
+
+        <div class="relative max-w-3xl mx-auto pb-96 px-4 text-center sm:pb-0 sm:px-6 lg:px-8">
+          <!-- Background image and overlap -->
+          <div aria-hidden="true" class="absolute inset-0 flex flex-col sm:hidden">
+            <div class="flex-1 relative w-full bg-gray-800">
+              <div class="absolute inset-0 overflow-hidden">
+                <img src="https://tailwindui.com/img/ecommerce-images/home-page-04-hero-full-width.jpg" alt="" class="w-full h-full object-center object-cover">
+              </div>
+              <div class="absolute inset-0 bg-gray-900 opacity-50"></div>
+            </div>
+            <div class="w-full bg-white h-48"></div>
+          </div>
+          <div class="relative py-32">
+            <h1 class="text-4xl font-extrabold tracking-tight text-white sm:text-5xl md:text-6xl">Kuchyně na míru</h1>
+            <div class="mt-4 sm:mt-6">
+              <a href="#" class="inline-block bg-indigo-600 border border-transparent rounded-md py-3 px-8 font-medium text-white hover:bg-indigo-700">Po kliknutí na obrázek budete pokračovat na galerii</a>
+            </div>
+          </div>
+        </div>
+
+        <section aria-labelledby="collection-heading" class="-mt-96 relative sm:mt-0">
+          <h2 id="collection-heading" class="sr-only">Collections</h2>
+          <div class="max-w-md mx-auto grid grid-cols-1 gap-y-6 px-4 sm:max-w-full sm:px-6 sm:grid-cols-3 sm:gap-y-0 sm:gap-x-6 lg:px-8 lg:gap-x-8">
+            
+              <div class="group relative h-96 bg-white rounded-lg shadow-xl sm:h-auto sm:aspect-w-4 sm:aspect-h-5">
+                <div>
+                  <div aria-hidden="true" class="absolute inset-0 rounded-lg overflow-hidden">
+                    <div class="absolute inset-0 overflow-hidden group-hover:opacity-75">
+                      <img src="https://tailwindui.com/img/ecommerce-images/home-page-04-collection-01.jpg" alt="Woman wearing a comfortable cotton t-shirt." class="w-full h-full object-center object-cover">
+                    </div>
+                    <div class="absolute inset-0 bg-gradient-to-b from-transparent to-black opacity-50"></div>
+                  </div>
+                  <div class="absolute inset-0 rounded-lg p-6 flex items-end">
+                    <div>
+                      <p aria-hidden="true" class="text-sm font-semibold text-white text-4xl">
+                        Klasické
+                      </p>
+                      <p class="mt-1 text-white text-xl">
+                        <a href="#">
+                          <span class="absolute inset-0"></span>
+                          Pokračovat na galerii
+                        </a>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            
+              <div class="group relative h-96 bg-white rounded-lg shadow-xl sm:h-auto sm:aspect-w-4 sm:aspect-h-5">
+                <div>
+                  <div aria-hidden="true" class="absolute inset-0 rounded-lg overflow-hidden">
+                    <div class="absolute inset-0 overflow-hidden group-hover:opacity-75">
+                      <img src="https://tailwindui.com/img/ecommerce-images/home-page-04-collection-02.jpg" alt="Man wearing a comfortable and casual cotton t-shirt." class="w-full h-full object-center object-cover">
+                    </div>
+                    <div class="absolute inset-0 bg-gradient-to-b from-transparent to-black opacity-50"></div>
+                  </div>
+                  <div class="absolute inset-0 rounded-lg p-6 flex items-end">
+                    <div>
+                      <p aria-hidden="true" class="text-sm font-semibold text-white text-4xl">
+                        Moderní
+                      </p>
+                      <p class="mt-1 text-white text-xl">
+                        <a href="#">
+                          <span class="absolute inset-0"></span>
+                          Pokračovat na galerii
+                        </a>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            
+              <div class="group relative h-96 bg-white rounded-lg shadow-xl sm:h-auto sm:aspect-w-4 sm:aspect-h-5">
+                <div>
+                  <div aria-hidden="true" class="absolute inset-0 rounded-lg overflow-hidden">
+                    <div class="absolute inset-0 overflow-hidden group-hover:opacity-75">
+                      <img src="https://tailwindui.com/img/ecommerce-images/home-page-04-collection-03.jpg" alt="Person sitting at a wooden desk with paper note organizer, pencil and tablet." class="w-full h-full object-center object-cover">
+                    </div>
+                    <div class="absolute inset-0 bg-gradient-to-b from-transparent to-black opacity-50"></div>
+                  </div>
+                  <div class="absolute inset-0 rounded-lg p-6 flex items-end">
+                    <div>
+                      <p aria-hidden="true" class="text-sm font-semibold text-white text-4xl">
+                        Bezúchytkové
+                      </p>
+                      <p class="mt-1 text-white text-xl">
+                        <a href="#">
+                          <span class="absolute inset-0"></span>
+                          Pokračovat na galerii
+                        </a>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            
+          </div>
+        </section>
+      </div>
+    </main>
 </template>
 
 <script>
@@ -39,13 +152,13 @@ export default {
         const category = ref()
 
         async function callApi() {
-            await api.getGalleries(route.query.category).then(response => {
-                    galleries.value = response.data
-                    category.value = response.data[0].category.name
-                }
-            ).catch(() => (
-                router.push({name: 'homeIndex'})
-            ))
+           // await api.getGalleries(route.query.category).then(response => {
+           //         galleries.value = response.data
+           //         category.value = response.data[0].category.name
+           //    }
+           // ).catch(() => (
+                //router.push({name: 'homeIndex'})
+           // ))
         }
 
         onMounted(() => {
--- resources/js/components/Public/Menu.vue
+++ resources/js/components/Public/Menu.vue
@@ -1,175 +1,157 @@
 <template>
-    <div class="min-h-full w-full bg-mycolor" id="hero">
-        <header id="navbar" class="bg-mycolor fixed inset-x-0 top-0 z-50 w-screen">
-            <div class="relative">
-                <div class="flex justify-between items-center max-w-7xl mx-auto py-6 md:justify-start">
-                    <div class="flex justify-start lg:w-0 lg:flex-1">
-                        <router-link :to="{ name: 'homeIndex', hash: '#home' }">
-                            <span class="sr-only">Workflow</span>
-                            <img class="h-8 w-auto sm:h-10 m-5" src="/storage/images/initialize/logo.png" alt=""/>
-                        </router-link>
-                    </div>
-                    <div class="-my-2 md:hidden">
-                        <div @click="showMenu = true"
-                            class="rounded-md p-2 m-5 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-                            <span class="sr-only">Open menu</span>
-                            <MenuIcon class="h-6 w-6" aria-hidden="true"/>
-                        </div>
-                    </div>
-                    <nav as="nav" class="hidden md:flex space-x-10">
-                        <div class="relative">
-                            <button @click="showMenu = !showMenu"
-                                :class="[showMenu ? 'text-gray-900' : 'text-gray-500', 'group bg-mycolor rounded-md inline-flex items-center text-base font-medium hover:text-gray-900']">
-                                <span>Vyrábíme</span>
-                                <ChevronDownIcon
-                                    :class="[showMenu ? 'text-gray-600' : 'text-gray-400', 'ml-2 h-5 w-5 group-hover:text-gray-500']"
-                                    aria-hidden="true"/>
-                            </button>
-
-                            <transition enter-active-class="transition ease-out duration-200"
-                                        enter-from-class="opacity-0 translate-y-1"
-                                        enter-to-class="opacity-100 translate-y-0"
-                                        leave-active-class="transition ease-in duration-150"
-                                        leave-from-class="opacity-100 translate-y-0"
-                                        leave-to-class="opacity-0 translate-y-1">
-                                <nav v-show="showMenu"
-                                    class="absolute z-10 -ml-4 mt-3 transform w-screen max-w-md lg:max-w-2xl lg:ml-0 lg:left-1/2 lg:-translate-x-1/2 ">
-                                    <div class="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
-                                        <div
-                                            class="relative grid gap-6 bg-mycolor px-5 py-6 sm:gap-8 sm:p-8 lg:grid-cols-2 px-2">
-                                            <router-link
-                                                v-for="item in solutions"
-                                                :key="item.name"
-                                                :to="{ name: 'galleriesIndex', query:{category: item.category}, hash: '#hero' }"
-                                                class="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50">
-                                                <div
-                                                    class="flex-shrink-0 flex items-center justify-center h-10 w-10 rounded-md bg-indigo-600 text-white sm:h-12 sm:w-12">
-                                                    <component :is="item.icon" class="h-6 w-6" aria-hidden="true"/>
-                                                </div>
-                                                <div class="ml-4">
-                                                    <p class="text-base font-medium text-gray-900">
-                                                        {{ item.name }}
-                                                    </p>
-                                                    <p class="mt-1 text-sm text-gray-500">
-                                                        {{ item.description }}
-                                                    </p>
-                                                </div>
-                                            </router-link>
-                                        </div>
-                                    </div>
-                                </nav>
-                            </transition>
-                        </div>
-                        <router-link v-for="item in navigation" :to="{ name: 'homeIndex', hash: item.href }"
-                                     :key="item.name" class="text-base font-medium text-gray-500 hover:text-gray-900">
-                            {{ item.name }}
-                        </router-link>
-                    </nav>
-                    <div class="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
-                        <!--
-                          !!!!Maybe in the future. Ready for login and user registration!!!!
-                          <a href="#" class="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
-                           Sign in
-                         </a>
-                         <a href="#" class="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">
-                           Sign up
-                         </a>
-                         -->
-                    </div>
-                </div>
-
-                <transition enter-active-class="duration-200 ease-out" enter-from-class="opacity-0 scale-95"
-                            enter-to-class="opacity-100 scale-100" leave-active-class="duration-100 ease-in"
-                            leave-from-class="opacity-100 scale-100" leave-to-class="opacity-0 scale-95">
-                    <div v-show="showMenu"
-                                  class="absolute z-30 top-0 inset-x-0 px-2 transition transform origin-top-right md:hidden">
-                        <div
-                            class="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-mycolor divide-y-2 divide-gray-50">
-                            <div class="pt-5 pb-6">
-                                <div class="flex items-center justify-between">
-                                    <router-link :to="{ name: 'homeIndex', hash: '#home' }">
-                                        <div>
-                                            <img class="h-8 w-auto m-5" src="/storage/images/initialize/logo.png" alt="Workflow" />
-                                        </div>
-                                    </router-link>
-                                    <div>
-                                        <button @click="showMenu = false"
-                                            class="bg-mycolor rounded-md m-5 p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-                                            <span class="sr-only">Close menu</span>
-                                            <XIcon class="h-6 w-6" aria-hidden="true"/>
-                                        </button>
-                                    </div>
-                                </div>
-                                <div class="mt-6">
-                                    <nav class="grid grid-cols-1 gap-7 px-2">
-                                        <router-link
-                                            v-for="item in solutions"
-                                            :key="item.name"
-                                            :to="{ name: 'galleriesIndex', query:{category: item.category}, hash: '#hero' }"
-                                            class="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50">
-                                            <div
-                                                class="flex-shrink-0 flex items-center justify-center h-10 w-10 rounded-md bg-indigo-600 text-white">
-                                                <component :is="item.icon" class="h-6 w-6" aria-hidden="true"/>
-                                            </div>
-                                            <div class="ml-4 text-base font-medium text-gray-900">
-                                                {{ item.name }}
-                                            </div>
-                                        </router-link>
-                                    </nav>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </transition>
+  <Disclosure as="header" class="bg-gray-800" v-slot="{ open }">
+    <div class="max-w-7xl mx-auto px-2 sm:px-4 lg:divide-y lg:divide-gray-700 lg:px-8 pt-10">
+      <div class="relative h-auto flex justify-between pb-16">
+        <div class="relative z-10 px-2 flex lg:px-0">
+          <div class="flex-shrink-0 flex items-center">
+            <div>
+                <p class="text-white text-base mb-2">Email:    truwork@truwork.com</p>
+                <p class="text-white text-base mb-2">Telefon:  607 237 514</p>
+                <p class="text-white text-base mb-2">Telefon:  607 237 514</p>
+                <p class="text-white text-base">Adresa : Jeseník 790 00, Astur 185</p>
             </div>
-        </header>
+          </div>
+        </div>
+        <div class="relative z-0 flex-1 px-2 flex items-center justify-center sm:absolute sm:inset-0">
+          <div class="w-full sm:max-w-xs">
+            <label for="search" class="sr-only">Search</label>
+            <div class="relative">
+              <div class="pointer-events-none absolute inset-y-0 left-0 pl-3 flex items-center">
+                <SearchIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
+              </div>
+              <img src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg" alt="Workflow" class="block h-16 w-full pb-2" />
+              <p class="text-white text-xl mb-2 text-center">Zakázková výroba na míru</p>
+            </div>
+          </div>
+        </div>
+        <div class="relative z-10 flex items-center lg:hidden">
+          <!-- Mobile menu button -->
+          <DisclosureButton class="rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+            <span class="sr-only">Open menu</span>
+            <MenuIcon v-if="!open" class="block h-6 w-6" aria-hidden="true" />
+            <XIcon v-else class="block h-6 w-6" aria-hidden="true" />
+          </DisclosureButton>
+        </div>
+        <div class="hidden lg:relative lg:z-10 lg:ml-4 lg:flex lg:items-center">
+          <button type="button" class="block bg-gray-800 flex-shrink-0 rounded-full p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+            <a v-for="item in social" :key="item.name" :href="item.href" :class="['text-gray-300 hover:bg-gray-700 hover:text-white', 'rounded-md py-2 px-3 inline-flex items-center text-sm font-medium']" :aria-current="item.current ? 'page' : undefined">
+                <component :is="item.icon" class="h-10 w-10" aria-hidden="true" />
+            </a>
+          </button>
+
+          <!-- Profile dropdown -->
+          <Menu as="div" class="flex-shrink-0 relative ml-4">
+            <div>
+              <MenuButton class="bg-gray-800 rounded-full flex text-sm text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+                <span class="sr-only">Open user menu</span>
+                <!-- //<img class="h-8 w-8 rounded-full" :src="user.imageUrl" alt="" /> -->
+              </MenuButton>
+            </div>
+            <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
+              <MenuItems class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 py-1 focus:outline-none">
+                <MenuItem v-for="item in userNavigation" :key="item.name" v-slot="{ active }">
+                  <a :href="item.href" :class="[active ? 'bg-gray-100' : '', 'block py-2 px-4 text-sm text-gray-700']">{{ item.name }}</a>
+                </MenuItem>
+              </MenuItems>
+            </transition>
+          </Menu>
+        </div>
+      </div>
+      <nav class="hidden lg:py-2 lg:flex lg:space-x-8 justify-center" aria-label="Global">
+        <a v-for="item in navigation" :key="item.name" :href="item.href" :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'rounded-md py-4 px-3 inline-flex items-center text-base font-medium']" :aria-current="item.current ? 'page' : undefined">
+          {{ item.name }}
+        </a>
+      </nav>
     </div>
+
+    <DisclosurePanel as="nav" class="lg:hidden" aria-label="Global">
+      <div class="pt-2 pb-3 px-2 space-y-1">
+        <DisclosureButton v-for="item in navigation" :key="item.name" as="a" :href="item.href" :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'block rounded-md py-2 px-3 text-base font-medium']" :aria-current="item.current ? 'page' : undefined">{{ item.name }}</DisclosureButton>
+      </div>
+      <div class="border-t border-gray-700 pt-4 pb-3">
+        <div class="px-4 flex items-center">
+          <div class="flex-shrink-0">
+            <img class="h-10 w-10 rounded-full" :src="user.imageUrl" alt="" />
+          </div>
+          <div class="ml-3">
+            <div class="text-base font-medium text-white">{{ user.name }}</div>
+            <div class="text-sm font-medium text-gray-400">{{ user.email }}</div>
+          </div>
+          <a v-for="item in social" :key="item.name" :href="item.href" class="text-gray-400 hover:text-gray-300">
+            <span class="sr-only">{{ item.name }}</span>
+            <component :is="item.icon" class="h-6 w-6" aria-hidden="true" />
+          </a>
+        </div> 
+      </div>
+    </DisclosurePanel>
+  </Disclosure>
 </template>
 
 <script>
-import {ref,watch} from 'vue'
+import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
+import {ref,watch,defineComponent, h} from 'vue'
 import {useRoute} from 'vue-router'
 import {
     CheckIcon,
     MenuIcon,
     XIcon,
+    BellIcon,
 } from '@heroicons/vue/outline'
 import {ChevronDownIcon} from '@heroicons/vue/solid'
 
-const solutions = [
-    {
-        name: 'Kuchyně',
-        description: 'Kuchyně na míru.',
-        category: 'kuchyne',
-        icon: CheckIcon,
-    },
-    {
-        name: 'Vestavěné skříně',
-        description: 'Vestavěné skříně na míru.',
-        category: 'vestavene_skrine',
-        icon: CheckIcon,
-    },
-    {
-        name: 'Komerční prostory',
-        description: "Vybavíme školy, školky, obecní úřády, restaurace, a další.",
-        category: 'komercni_prostory',
-        icon: CheckIcon
-    },
-    {
-        name: 'Ostatní',
-        description: "Pergoly, dveře, přistřešky.",
-        category: 'ostatni',
-        icon: CheckIcon,
-    },
-]
 const navigation = [
-    {name: 'O nás', href: '#about-us'},
-    {name: 'Reference', href: '#'},
+    {name: 'Domů', href: '#about-us'},
+    {name: 'Kuchyně', href: '#'},
+    {name: 'Skříně', href: '#contact-as'},
+    {name: 'Komerční prostory', href: '#about-us'},
+    {name: 'Koupelny', href: '#'},
+    {name: 'Doplňky/Detaily', href: '#contact-as'},
     {name: 'Kontakt', href: '#contact-as'},
 ]
 
+const social = [
+    {
+      name: 'Facebook',
+      href: '#',
+      icon: defineComponent({
+        render: () =>
+          h('svg', { fill: 'currentColor', viewBox: '0 0 24 24' }, [
+            h('path', {
+              'fill-rule': 'evenodd',
+              d:
+                'M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z',
+              'clip-rule': 'evenodd',
+            }),
+          ]),
+      }),
+    },
+    {
+      name: 'Instagram',
+      href: '#',
+      icon: defineComponent({
+        render: () =>
+          h('svg', { fill: 'currentColor', viewBox: '0 0 24 24' }, [
+            h('path', {
+              'fill-rule': 'evenodd',
+              d:
+                'M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z',
+              'clip-rule': 'evenodd',
+            }),
+          ]),
+      }),
+    }
+  ]
+
 export default {
     components: {
+        Disclosure,
+        DisclosureButton,
+        DisclosurePanel,
+        Menu,
+        MenuButton,
+        MenuItem,
+        MenuItems,
+        BellIcon,
         ChevronDownIcon,
         MenuIcon,
         XIcon,
@@ -184,9 +166,9 @@ export default {
             }
         });
         return {
-            solutions,
             navigation,
             showMenu,
+            social,
         }
     },
 }
