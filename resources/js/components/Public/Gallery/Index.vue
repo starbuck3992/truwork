5f7abd8de4f760f7d5eb00f38242238c3d057a10
svardys <75584296+svardys@users.noreply.github.com>
2021-12-21 18:48:08 +0100

    FE

--- resources/js/components/Public/Gallery/Index.vue
+++ resources/js/components/Public/Gallery/Index.vue
@@ -20,7 +20,8 @@
     </div> -->
 <main>
   <div v-for="(gallery, i) in galleries" :key="i" class="bg-white">
-    <router-link :to="{ name: 'galleriesShow', params: {slug: gallery.slug } }" class="max-w-2xl mx-auto py-4 px-4 sm:px-6 lg:max-w-7xl">
+    <router-link :to="{ name: 'galleriesShow', params: {slug: gallery.slug } }" class="w-full h-full">
+    <div class="max-w-2xl mx-auto py-4 px-4 sm:px-6 lg:max-w-7xl">
       <div class="relative rounded-lg overflow-hidden lg:h-128">
         <div class="absolute inset-0">
           <img :src="gallery.thumbnail[0].path" :alt="category" class="w-full h-full object-center object-cover" />
@@ -30,13 +31,14 @@
         <div class="absolute inset-x-0 bottom-0 bg-black bg-opacity-75 p-6 rounded-bl-lg rounded-br-lg backdrop-filter backdrop-blur sm:flex sm:items-center sm:justify-between lg:inset-y-0 lg:inset-x-auto lg:w-96 lg:rounded-tl-lg lg:rounded-br-none lg:flex-col lg:items-start">
           <div>
             <h2 class="text-xl font-bold text-white">{{ gallery.title }}</h2>
-            <p class="mt-1 text-sm text-gray-300">Popisek Čehokoli, od kuchyně po komodu :D.</p>
+            <p class="mt-1 text-sm hidden md:block text-gray-300">{{ gallery.description }}</p>
           </div>
           <a href="#" class="mt-6 flex-shrink-0 flex bg-white bg-opacity-0 py-3 px-4 border border-white border-opacity-25 rounded-md items-center justify-center text-base font-medium text-white hover:bg-opacity-10 sm:mt-0 sm:ml-8 lg:ml-0 lg:w-full">
-              {{ gallery.description }}
+             Pokračovat na galerii
           </a>
         </div>
       </div>
+    </div>
     </router-link>
   </div>
   
